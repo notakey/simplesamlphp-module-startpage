c631e47a573782c79aebe0600a66576e871dd0dc
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2019-04-05 16:25:59 +0300

    Add default logo for SPs

--- templates/splist.tpl.php
+++ templates/splist.tpl.php
@@ -1,17 +1,15 @@
 <?php
-
-
 $this->includeAtTemplateBase('includes/header.php');
 ?>
-<?php if($this->data['config']['pageTitle'] != ''){?><h2 style="break: both"><?php echo $this->data['config']['pageTitle']; ?></h2><?php } ?>
-<?php if($this->data['config']['pageSubtitle'] != ''){?><p><?php echo $this->data['config']['pageSubtitle']; ?></p><?php } ?>
+<?php if($this->data['config']['pageTitle'] != ''){?><h2 style="break: both"><?php echo htmlspecialchars($this->data['config']['pageTitle']); ?></h2><?php } ?>
+<?php if($this->data['config']['pageSubtitle'] != ''){?><p><?php echo htmlspecialchars($this->data['config']['pageSubtitle']); ?></p><?php } ?>
 
 <div id="sp-listing">
 	<?php foreach($this->data['spl'] as $sp){ ?>
 	<div class="sp-block">
 		<div class="sp-block-image">
 			<a class="sp-block-image-link" href="<?php echo $sp['link']; ?>">
-	        	<img src="/userlogos/<?php echo $sp['logo']; ?>">
+	        	<img src="<?php echo htmlspecialchars($sp['logo']); ?>">
 	        </a>
 	    </div>
 		<div class="sp-block-title">
@@ -23,9 +21,5 @@ $this->includeAtTemplateBase('includes/header.php');
 </div>
 <div style="clear:both;"></div>
 
-
 <?php
-
-
-
 $this->includeAtTemplateBase('includes/footer.php');
