1eeeaeb9e315ffbd4868c6d5b8ba56e599aa0401
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2020-04-06 22:48:29 +0300

    Formatting

--- www/splist.php
+++ www/splist.php
@@ -1,23 +1,23 @@
 <?php
 
 $config = SimpleSAML_Configuration::getInstance();
-$spconfig = \SimpleSAML_Configuration::getOptionalConfig( 'module_startpage.php' );
+$spconfig = \SimpleSAML_Configuration::getOptionalConfig('module_startpage.php');
 
-$copts = [ 's:pageTitle', 's:pageSubtitle', 'b:showLogout', 's:forgotPasswordUrl', 's:helpUrl' ];
+$copts = ['s:pageTitle', 's:pageSubtitle', 'b:showLogout', 's:forgotPasswordUrl', 's:helpUrl'];
 
 $t = new SimpleSAML_XHTML_Template($config, 'startpage:splist.tpl.php');
 $t->data['header'] = $t->t('{startpage:startpage:splist_header}');
 $t->data['pageid'] = 'splist';
 
-$t->data['head']  = '<link rel="stylesheet" type="text/css" href="/'. $t->data['baseurlpath']. 'module/startpage/resources/style.css" />';
+$t->data['head']  = '<link rel="stylesheet" type="text/css" href="/' . $t->data['baseurlpath'] . 'module/startpage/resources/style.css" />';
 
 $t->data['spl'] = array();
 
 $t->data['config'] = array();
 
-foreach($copts as $c){
+foreach ($copts as $c) {
     list($dataType, $confOpt) = explode(':', $c, 2);
-    switch($dataType){
+    switch ($dataType) {
         case "s":
             $t->data['config'][$confOpt] = $spconfig->getString($confOpt, "");
             break;
@@ -25,34 +25,32 @@ foreach($copts as $c){
             $t->data['config'][$confOpt] = $spconfig->getBoolean($confOpt, "");
             break;
     }
-
 }
 
 $metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
 
 $metaentries = array_merge($metadata->getList('saml20-sp-remote'), $metadata->getList('shib13-sp-remote'), $metadata->getList('adfs-sp-remote'));
 
-foreach($metaentries as $spentityid=>$c){
-	// TODO
-	// Fix to be multilang compatible
-	if(is_array($c['name'])){
-		$name = array_pop($c['name']);
-	}else{
-		$name = $c['name'];
-	}
+foreach ($metaentries as $spentityid => $c) {
+    // TODO
+    // Fix to be multilang compatible
+    if (is_array($c['name'])) {
+        $name = array_pop($c['name']);
+    } else {
+        $name = $c['name'];
+    }
 
-	if(is_array($c['startpage.logo'])){
-		$logo = '/userlogos/'.$c['startpage.logo']['en'];
-	}else{
-        if(isset($c['startpage.logo']) && !empty($c['startpage.logo']) && $c['startpage.logo'] != 'nologo.png'){
-            $logo = '/userlogos/'.$c['startpage.logo'];
-        }else{
+    if (is_array($c['startpage.logo'])) {
+        $logo = '/userlogos/' . $c['startpage.logo']['en'];
+    } else {
+        if (isset($c['startpage.logo']) && !empty($c['startpage.logo']) && $c['startpage.logo'] != 'nologo.png') {
+            $logo = '/userlogos/' . $c['startpage.logo'];
+        } else {
             $logo = '/sso/module.php/startpage/resources/sad_cat_sso.png';
         }
-	}
+    }
 
-	$t->data['spl'][] = array('link' => $c['startpage.link'], 'name' => $name, 'logo' => $logo);
+    $t->data['spl'][] = array('link' => $c['startpage.link'], 'name' => $name, 'logo' => $logo);
 }
 
 $t->show();
-
